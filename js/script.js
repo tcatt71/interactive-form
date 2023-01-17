d610359f1f678bc8fd1d4838949dff071fa95250
Troy Coffindaffer <61867285+tcatt71@users.noreply.github.com>
2023-01-17 02:27:35 -0800

    Use the HTMLElement hidden property to hide it on load

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 const nameField = document.querySelector('#name');
 const otherJobRoleField = document.querySelector('#other-job-role');
+const jobRoleDropDownMenu = document.querySelector('#title');
 
 nameField.focus();
-otherJobRoleField.style.display = 'none';
+otherJobRoleField.hidden = true;
