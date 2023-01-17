83272ad8e03fd704875dc6c04565a44d5643dc0c
Troy Coffindaffer <61867285+tcatt71@users.noreply.github.com>
2023-01-17 01:58:56 -0800

    Hide the other-job-role field when the page loads

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
 const nameField = document.querySelector('#name');
+const otherJobRoleField = document.querySelector('#other-job-role');
 
 nameField.focus();
+otherJobRoleField.style.display = 'none';
