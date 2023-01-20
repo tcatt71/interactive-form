1b59977c7cc8a663eb3d56647e9749994794f113
Troy Coffindaffer <61867285+tcatt71@users.noreply.github.com>
2023-01-20 02:55:20 -0800

    Disable the color drop down menu on load

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const nameField = document.querySelector('#name');
 const otherJobRoleField = document.querySelector('#other-job-role');
 const jobRoleDropDownMenu = document.querySelector('#title');
+const colorDropDownMenu = document.querySelector('#color');
 
 nameField.focus();
 otherJobRoleField.hidden = true;
@@ -14,3 +15,5 @@ function toggleOtherJobRoleField() {
     otherJobRoleField.hidden = true;
   }
 }
+
+colorDropDownMenu.disabled = true;
