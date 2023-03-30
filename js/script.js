b54be20a35fe780b7a8f5933fc63dd8a819fbc32
Troy Coffindaffer <61867285+tcatt71@users.noreply.github.com>
2023-03-30 01:21:05 -0700

    Rename parameter eventObj to event

--- js/script.js
+++ js/script.js
@@ -69,9 +69,9 @@ function selectTShirtColorByTheme() {
 
 registerForActivitesFieldset.addEventListener('change', updateCostOfAttendance);
 
-function updateCostOfAttendance(eventObj) {
+function updateCostOfAttendance(event) {
   const activitiesCostParagraph = document.querySelector('#activities-cost');
-  const checkbox = eventObj.target;
+  const checkbox = event.target;
   const activityCost = parseInt(checkbox.dataset.cost);
   let totalCost = parseInt(activitiesCostParagraph.textContent.substring(8));
 
@@ -116,49 +116,49 @@ function dispalyPaymentOptionFields() {
 
 formElement.addEventListener('submit', validateForm);
 
-function validateForm(eventObj) {
-  if (nameIsValid(eventObj)) {
+function validateForm(event) {
+  if (nameIsValid(event)) {
     handleValidInput(nameField);
   } else {
-    handleInvalidInput(nameField, eventObj);
+    handleInvalidInput(nameField, event);
   }
 
-  if (emailIsValid(eventObj)) {
+  if (emailIsValid(event)) {
     handleValidInput(emailField);
   } else {
-    handleInvalidInput(emailField, eventObj);
+    handleInvalidInput(emailField, event);
   }
 
-  if (registerForActivitiesIsChecked(eventObj)) {
+  if (registerForActivitiesIsChecked(event)) {
     handleValidInput(activitiesBoxDiv);
   } else {
-    handleInvalidInput(activitiesBoxDiv, eventObj);
+    handleInvalidInput(activitiesBoxDiv, event);
   }
 
   if (!creditCardDiv.hidden) {
-    if (creditCardNumberIsValid(eventObj)) {
+    if (creditCardNumberIsValid(event)) {
       handleValidInput(creditCardNumberField);
     } else {
-      handleInvalidInput(creditCardNumberField, eventObj);
+      handleInvalidInput(creditCardNumberField, event);
     }
 
-    if (zipCodeIsValid(eventObj)) {
+    if (zipCodeIsValid(event)) {
       handleValidInput(zipCodeField);
     } else {
-      handleInvalidInput(zipCodeField, eventObj);
+      handleInvalidInput(zipCodeField, event);
     }
 
-    if (cvvIsValid(eventObj)) {
+    if (cvvIsValid(event)) {
       handleValidInput(cvvField);
     } else {
-      handleInvalidInput(cvvField, eventObj);
+      handleInvalidInput(cvvField, event);
     }
   }
 }
 
 nameField.addEventListener('keyup', nameIsValid);
 
-function nameIsValid(eventObj) {
+function nameIsValid(event) {
   const name = nameField.value;
 
   const isValid = /\S/.test(name)
@@ -166,14 +166,14 @@ function nameIsValid(eventObj) {
   if (isValid) {
     handleValidInput(nameField);
   } else {
-    handleInvalidInput(nameField, eventObj);
+    handleInvalidInput(nameField, event);
   }
   return isValid;
 }
 
 emailField.addEventListener('keyup', emailIsValid);
 
-function emailIsValid(eventObj) {
+function emailIsValid(event) {
   const email = emailField.value;
 
   const isValid = /^\w+\.?\w+@\w+\.(com|net|org)$/i.test(email);
@@ -182,17 +182,17 @@ function emailIsValid(eventObj) {
     handleValidInput(emailField);
   } else if (email === '') {
     emailField.parentElement.lastElementChild.textContent = 'Please enter an email address';
-    handleInvalidInput(emailField, eventObj);
+    handleInvalidInput(emailField, event);
   } else {
     emailField.parentElement.lastElementChild.textContent = 'Email address must be formatted correctly';
-    handleInvalidInput(emailField, eventObj);
+    handleInvalidInput(emailField, event);
   }
   return isValid;
 }
 
 activitiesBoxDiv.addEventListener('change', registerForActivitiesIsChecked);
 
-function registerForActivitiesIsChecked(eventObj) {
+function registerForActivitiesIsChecked(event) {
   const checkboxes = document.querySelectorAll('#activities-box [type="checkbox"]');
   let isValid = false;
 
@@ -205,14 +205,14 @@ function registerForActivitiesIsChecked(eventObj) {
   if (isValid) {
     handleValidInput(activitiesBoxDiv);
   } else {
-    handleInvalidInput(activitiesBoxDiv, eventObj);
+    handleInvalidInput(activitiesBoxDiv, event);
   }
   return isValid;
 }
 
 creditCardNumberField.addEventListener('keyup', creditCardNumberIsValid);
 
-function creditCardNumberIsValid(eventObj) {
+function creditCardNumberIsValid(event) {
   const creditCardNumber = creditCardNumberField.value;
 
   const isValid = /^\d{13,16}$/.test(creditCardNumber)
@@ -220,14 +220,14 @@ function creditCardNumberIsValid(eventObj) {
   if (isValid) {
     handleValidInput(creditCardNumberField);
   } else {
-    handleInvalidInput(creditCardNumberField, eventObj);
+    handleInvalidInput(creditCardNumberField, event);
   }
   return isValid;
 }
 
 zipCodeField.addEventListener('keyup', zipCodeIsValid);
 
-function zipCodeIsValid(eventObj) {
+function zipCodeIsValid(event) {
   const zipCode = zipCodeField.value;
 
   const isValid = /^\d{5}$/.test(zipCode);
@@ -235,14 +235,14 @@ function zipCodeIsValid(eventObj) {
   if (isValid) {
     handleValidInput(zipCodeField);
   } else {
-    handleInvalidInput(zipCodeField, eventObj);
+    handleInvalidInput(zipCodeField, event);
   }
   return isValid;
 }
 
 cvvField.addEventListener('keyup', cvvIsValid);
 
-function cvvIsValid(eventObj) {
+function cvvIsValid(event) {
   const cvv = cvvField.value;
 
   const isValid = /^\d{3}$/.test(cvv);
@@ -250,7 +250,7 @@ function cvvIsValid(eventObj) {
   if (isValid) {
     handleValidInput(cvvField);
   } else {
-    handleInvalidInput(cvvField, eventObj);
+    handleInvalidInput(cvvField, event);
   }
   return isValid;
 }
@@ -261,8 +261,8 @@ function handleValidInput(element) {
   element.parentElement.lastElementChild.style.display = 'none';
 }
 
-function handleInvalidInput(element, eventObj) {
-  eventObj.preventDefault();
+function handleInvalidInput(element, event) {
+  event.preventDefault();
   element.parentElement.classList.remove('valid');
   element.parentElement.classList.add('not-valid');
   element.parentElement.lastElementChild.style.display = 'block';
